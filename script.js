10a4a11a4fe72099c5fc88d22d1e9bd377a1f83d
VincentSequerah <v1jnicholas@gmail.com>
2024-10-16 23:25:33 +0700

    adding functionality to all keys

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const A = new Audio("piano-mp3/A0.mp3");
+const A1 = new Audio("piano-mp3/A3.mp3");
+const Ab1 = new Audio("piano-mp3/A3.mp3");
+const B1 = new Audio("piano-mp3/A3.mp3");
+const C1 = new Audio("piano-mp3/A3.mp3");
+const Cb1 = new Audio("piano-mp3/A3.mp3");
+const D1 = new Audio("piano-mp3/A3.mp3");
+const Db1 = new Audio("piano-mp3/A3.mp3");
+const E1 = new Audio("piano-mp3/A3.mp3");
+const F1 = new Audio("piano-mp3/A3.mp3");
+const Fb1 = new Audio("piano-mp3/A3.mp3");
+const G1 = new Audio("piano-mp3/A3.mp3");
+const Gb1 = new Audio("piano-mp3/A3.mp3");
+const A2 = new Audio("piano-mp3/A3.mp3");
+const Ab2 = new Audio("piano-mp3/A3.mp3");
+const B2 = new Audio("piano-mp3/A3.mp3");
+const C2 = new Audio("piano-mp3/A3.mp3");
+const Cb2 = new Audio("piano-mp3/A3.mp3");
+const D2 = new Audio("piano-mp3/A3.mp3");
+const Db2 = new Audio("piano-mp3/A3.mp3");
+const E2 = new Audio("piano-mp3/A3.mp3");
+const F2 = new Audio("piano-mp3/A3.mp3");
+const Fb2 = new Audio("piano-mp3/A3.mp3");
+const G2 = new Audio("piano-mp3/A3.mp3");
+const Gb2 = new Audio("piano-mp3/A3.mp3");
 
 
 
-function note(){
 
-    A.play();
+function note(n){
+
+    n.play();
 }
