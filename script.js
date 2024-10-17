052349b9b103b1e9ea77acd8a8fba506ee7ce03c
VincentSequerah <v1jnicholas@gmail.com>
2024-10-17 23:33:59 +0700

    update

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 const A1 = new Audio("piano-mp3/A3.mp3");
-const Ab1 = new Audio("piano-mp3/A3.mp3");
-const B1 = new Audio("piano-mp3/A3.mp3");
-const C1 = new Audio("piano-mp3/A3.mp3");
-const Cb1 = new Audio("piano-mp3/A3.mp3");
-const D1 = new Audio("piano-mp3/A3.mp3");
-const Db1 = new Audio("piano-mp3/A3.mp3");
-const E1 = new Audio("piano-mp3/A3.mp3");
-const F1 = new Audio("piano-mp3/A3.mp3");
-const Fb1 = new Audio("piano-mp3/A3.mp3");
-const G1 = new Audio("piano-mp3/A3.mp3");
-const Gb1 = new Audio("piano-mp3/A3.mp3");
-const A2 = new Audio("piano-mp3/A3.mp3");
-const Ab2 = new Audio("piano-mp3/A3.mp3");
-const B2 = new Audio("piano-mp3/A3.mp3");
-const C2 = new Audio("piano-mp3/A3.mp3");
-const Cb2 = new Audio("piano-mp3/A3.mp3");
-const D2 = new Audio("piano-mp3/A3.mp3");
-const Db2 = new Audio("piano-mp3/A3.mp3");
-const E2 = new Audio("piano-mp3/A3.mp3");
-const F2 = new Audio("piano-mp3/A3.mp3");
-const Fb2 = new Audio("piano-mp3/A3.mp3");
-const G2 = new Audio("piano-mp3/A3.mp3");
-const Gb2 = new Audio("piano-mp3/A3.mp3");
+const Ab1 = new Audio("piano-mp3/Bb3.mp3");
+const B1 = new Audio("piano-mp3/B3.mp3");
+const C1 = new Audio("piano-mp3/C4.mp3");
+const Cb1 = new Audio("piano-mp3/Db4.mp3");
+const D1 = new Audio("piano-mp3/D4.mp3");
+const Db1 = new Audio("piano-mp3/Eb4.mp3");
+const E1 = new Audio("piano-mp3/E4.mp3");
+const F1 = new Audio("piano-mp3/F4.mp3");
+const Fb1 = new Audio("piano-mp3/Gb4.mp3");
+const G1 = new Audio("piano-mp3/G4.mp3");
+const Gb1 = new Audio("piano-mp3/Ab4.mp3");
+const A2 = new Audio("piano-mp3/A4.mp3");
+const Ab2 = new Audio("piano-mp3/Bb4.mp3");
+const B2 = new Audio("piano-mp3/B4.mp3");
+const C2 = new Audio("piano-mp3/C5.mp3");
+const Cb2 = new Audio("piano-mp3/Db5.mp3");
+const D2 = new Audio("piano-mp3/D5.mp3");
+const Db2 = new Audio("piano-mp3/Eb5.mp3");
+const E2 = new Audio("piano-mp3/E5.mp3");
+const F2 = new Audio("piano-mp3/F5.mp3");
+const Fb2 = new Audio("piano-mp3/Gb5.mp3");
+const G2 = new Audio("piano-mp3/G5.mp3");
+const Gb2 = new Audio("piano-mp3/Ab5.mp3");
+const A3 = new Audio("piano-mp3/A5.mp3");
 
 
 
 
 function note(n){
-
+    n.currentTime=0;
     n.play();
 }
